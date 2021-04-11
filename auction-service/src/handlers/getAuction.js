ee91e6a474a27c178c42cbfe50985f62f1577560
Mordi Goldstein <62645629+MGoldstein18@users.noreply.github.com>
2021-04-11 08:14:06 +0200

    added bid creation and validation

--- auction-service/src/handlers/getAuction.js
+++ auction-service/src/handlers/getAuction.js
@@ -1,12 +1,36 @@
-import AWS, { EnvironmentCredentials } from "aws-sdk";
+import AWS from "aws-sdk";
 import commonMiddleware from "../lib/commonMiddleware.js";
 import createError from "http-errors";
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
-async function getAuction(event, context) {
+export async function getAuctionById(id) {
   let auction;
+
+  try {
+    const result = await dynamodb
+      .get({
+        TableName: process.env.AUCTIONS_TABLE_NAME,
+        Key: { id },
+      })
+      .promise();
+
+    auction = result.Item;
+  } catch (error) {
+    console.error(error);
+    throw new createError.InternalServerError(error);
+  }
+
+  if (!auction) {
+    throw new createError.NotFound(`Auction with ID ${id} was not found!`);
+  }
+
+  return auction;
+}
+
+async function getAuction(event, context) {
   const { id } = event.pathParameters;
+  const auction = await getAuctionById(id);
 
   try {
     const result = await dynamodb
